584b74ee4213b0121cebab44624a9ec6fa08775c
Bashayer Nouri <bashayer_nouri@hotmail.com>
2019-09-01 11:52:19 +0300

    Farah and Bashayer

--- stores/admin.py
+++ stores/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Store
 
-# Register your models here.
-admin.site.register(Store)
+
+class StoreAdmin(admin.ModelAdmin):
+    # ...
+    list_display = ('id', 'name','description')
+    list_display_links = ('name','description')
+    list_filter = ['name','description']
+    search_fields = ['name']
+
+
+admin.site.register(Store,StoreAdmin)
+
